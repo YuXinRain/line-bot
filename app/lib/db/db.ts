cc39cee2550781582f5fa64675fbd2d7a1f0f0f2
rain <rain88414@gmail.com>
2026-03-30 16:49:22 +0800

    fix: database

--- app/lib/db/db.ts
+++ app/lib/db/db.ts
@@ -3,10 +3,11 @@ import mysql from 'mysql2/promise';
 
 // 建立連線池
 export const pool = mysql.createPool({
-  host: 'localhost',       // MySQL 主機
-  user: 'root',            // 你的帳號
-  password: 'rain0911',     // 你的密碼
-  database: 'line_bot',    // 你建立的資料庫名稱
+  host: process.env.DB_HOST,       // 可以寫死也可以用 env
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // 必須用 env
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
